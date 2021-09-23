27c93e70d6af3d13c3c4354ebb8548faa6874a34
Sudi Simba <sudisimbadav@gmail.com>
2021-09-23 22:23:56 +0200

    Add project files.

---
+++ SimbaLibrary.App/Models/Books/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace SimbaLibrary.App.Models.Books
+{
+    public class Book
+    {
+        public Guid Id {  get; set; }   
+        public string Title {  get; set; }
+        public string Description {  get; set; }
+        public string Author {  get; set; }
+        public string Price {  get; set; }
+        public int PageCount {  get; set; }
+        public DateTimeOffset ReleaseDate {  get; set;}
+        public bool InStock { get; set; }
+        public string ISBN { get; set; }
+    }
+}
